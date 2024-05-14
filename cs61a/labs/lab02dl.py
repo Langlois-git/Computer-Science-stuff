dfcffe012dfa6565bc0f4352750c71e91b8ba446
david <langois-git@outlook.com>
2024-05-14 11:30:12 -0700

    Added a labs folder

---
+++ cs61a/labs/lab02dl.py
@@ -0,0 +1,6 @@
+ print(True and 13) # <- This should evaluate to true because non-zero integers evaluate to true in python
+ print(False or 0) # <- This should evaluate to False because 0 is false in python
+ print(not 10) # <- This should evaluate to False because non-zero integers are True, not True = False
+ print(not None)# <- In python the None type evaluates to false so not False would equal True
+ print(True and 1/0) # <- This should return a DIVIDEBYZERO Error because 
+ print
