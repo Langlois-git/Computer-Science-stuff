6dc9adfc743dbe2735b77ccb9b60f9098c8ee2fb
david <langois-git@outlook.com>
2024-05-14 09:01:49 -0700

    quick save

--- cs61a/composing-programs/abstractions_and_first_class_functions.py
+++ cs61a/composing-programs/abstractions_and_first_class_functions.py
@@ -1,4 +1,4 @@
-# The rights and privelages of first class functions are 
+# The rights and privelages of first class functions are
 # 1. They may be bound to names 
 # 2. They may be passed as arguments to functions
 # 3. They may be returned as the results of functions
