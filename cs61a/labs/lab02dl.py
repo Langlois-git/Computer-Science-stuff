2e93d49fc1719c4d15d0dd90f515ec6869351781
david <langois-git@outlook.com>
2024-05-20 09:58:56 -0700

    completed and tested final lab bonus question

--- cs61a/labs/lab02dl.py
+++ cs61a/labs/lab02dl.py
@@ -9,7 +9,7 @@
 # print(2 and 1) #<- This should return True 
 
 from doctest import run_docstring_examples
-from operator import add
+from operator import add, mul, sub
 
 def composite_identity(f, g):
     """
@@ -146,3 +146,64 @@ def multiple(a, b):
     return small_total 
 
 run_docstring_examples(multiple, globals(), True)
+
+"""Define a function cycle that takes in three functions f1, f2, and f3, as arguments. cycle will return another function g that should take in an integer argument n and return another function h. That final function h should take in an argument x and cycle through applying f1, f2, and f3 to x, depending on what n was. Here's what the final function h should do to x for a few values of n:
+
+    n = 0, return x
+    n = 1, apply f1 to x, or return f1(x)
+    n = 2, apply f1 to x and then f2 to the result of that, or return f2(f1(x))
+    n = 3, apply f1 to x, f2 to the result of applying f1, and then f3 to the result of applying f2, or f3(f2(f1(x)))
+    n = 4, start the cycle again applying f1, then f2, then f3, then f1 again, or f1(f3(f2(f1(x))))
+    And so forth.
+
+Hint: most of the work goes inside the most nested function"""
+
+def cycle(f1, f2, f3):
+    """Returns a function that is itself a higher-order function.
+
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    def g(n):
+        def h(x):
+            function_count = 0
+            function_number = 1
+            while function_count < n:
+                if function_number > 3:
+                    function_number = 1
+                if function_number == 1 :
+                    x = f1(x)
+                elif function_number == 2:
+                    x = f2(x)
+                elif function_number == 3:
+                    x = f3(x)
+                function_number += 1
+                function_count += 1
+            return x 
+        return h
+    return g
+
+
+run_docstring_examples(cycle, globals(), True)
+
+
