f6df78ddbc5058fdb91d5302b96d5cf3fd3542f9
david <langois-git@outlook.com>
2024-05-22 14:32:29 -0600

    more lab3

--- cs61a/labs/lab03dl.py
+++ cs61a/labs/lab03dl.py
@@ -1,5 +1,14 @@
 from doctest import run_docstring_examples
-#Q2
+#Q2: Implement print_if, which takes a list s and a one-argument function f. it prints each element of x of s for which f(x) returns a True value
+
+def print_if(s, f):
+    """Print each element of s for which f returns a true value
+    
+    >>> print_if([3, 4, 5, 6],  lambda x: x < 4)
+    5
+    6
+    
+    """
 
 # Q3: Implement close_list, which takes a list of numbers s and a non-negative integer k. It returns a list of the elements of s that are within k of thier index. That is, the absolute value of the difference between the element and its index is less than or equal to k.
 
